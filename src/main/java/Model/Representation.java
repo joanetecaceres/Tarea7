5903b3e4ae9b9f65513d2d70153f926112304f0d
Joanete Caceres <jc.caceres@uniandes.edu.co>
2016-04-27 00:49:18 -0500

    Presentar Datos en Pantalla - Tarea 7

--- src/main/java/Model/Representation.java
+++ src/main/java/Model/Representation.java
@@ -19,8 +19,55 @@ public class Representation {
      * @return String with HTML tags to present a table in the browser
      */
     static public String htmlRepresentation(List<Result> results) {
-        String htmlRepresentation = "";
+        
+        String htmlRepresentation = "<table><tr><td> Resultado </td></tr> ";
+        
+        for(int j = 1 ; j < results.size() ;j++ ){         
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation += "Caso de Prueba " + j;            
+            htmlRepresentation +="</td></tr>";      
+                        
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation += "Rx,y: " + results.get(j).getCorrelationR();            
+            htmlRepresentation +="</td></tr>";      
+                               
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="R2: " +  results.get(j).getCorrelationSquareR();            
+            htmlRepresentation +="</td></tr>";  
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="Significance: " +  results.get(j).getExpSignificance();            
+            htmlRepresentation +="</td></tr>";     
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="B0: " +  results.get(j).getExpB0();            
+            htmlRepresentation +="</td></tr>";    
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="B1: " +  results.get(j).getExpB1();            
+            htmlRepresentation +="</td></tr>";    
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="Yk: " +  results.get(j).getExpYk();            
+            htmlRepresentation +="</td></tr>";    
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="Range: " +  results.get(j).getExpRange();            
+            htmlRepresentation +="</td></tr>";    
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="UPI (70%)-: " +  results.get(j).getExpUPI();            
+            htmlRepresentation +="</td></tr>";    
+            
+            htmlRepresentation +="<tr><td>";
+            htmlRepresentation +="LPI (70%): " +  results.get(j).getLPI();            
+            htmlRepresentation +="</td></tr>";    
+            
+        }       
+        
+        htmlRepresentation +="</table>";
+        
         return htmlRepresentation;
-    }
-    
+    }    
 }
